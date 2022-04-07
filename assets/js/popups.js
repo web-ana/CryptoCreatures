43b34f531eaf5e12e0f8c5af417bb15d8900a637
Giorgi Matsukatovi <giorgimatsukatov@gmail.com>
2022-04-07 16:02:41 +0400

    dhawudhwa

--- assets/js/popups.js
+++ assets/js/popups.js
@@ -7,3 +7,8 @@ function closepopup(popup) {
     document.querySelector('.popup-overlay').style.display = 'none';
     document.querySelector(popup).style.display = 'none';
 }
+
+setTimeout(function() {
+    document.querySelector('.popup-overlay').style.display = 'grid';
+    document.querySelector('.popup-flyer').style.display = 'block';
+}, 5000);
